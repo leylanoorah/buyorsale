a5a4606c7e4ef29980e1c07757dcea2774c4489f
Danniebrice <danniebrice@brandeis.edu>
2016-10-19 14:31:48 -0400

    comment.js

---
+++ pages/about/client/Comments.js
@@ -0,0 +1,11 @@
+Template.comments.events({
+  "click #submitcomment": function(event)}{
+     console.log("hey we clicked it");
+     alert("yay!!");
+  }
+})
+
+
+Template.comments.helpera({
+
+})
