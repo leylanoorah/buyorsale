ac228dadc45f3bd1fe01e1c002eaf45362f6c3fe
RicardoChino <Elprimitony@hotmail.com>
2016-10-25 14:18:54 -0400

    profile

--- pages/comments/client/comments.js
+++ pages/comments/client/comments.js
@@ -11,10 +11,6 @@ Template.comments.events({
 
 
 Template.comments.helpers({
-<<<<<<< HEAD:pages/about/client/Comments.js
+
   commentlist: function(){return Comments.find();}
 })
-=======
-
-})
->>>>>>> origin/master:pages/comments/client/comments.js
