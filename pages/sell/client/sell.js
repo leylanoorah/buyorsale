b0959479645e54f095800513257aec7b5ff7c9d8
RicardoChino <Elprimitony@hotmail.com>
2016-10-31 14:50:02 -0400

    im bored

---
+++ pages/sell/client/sell.js
@@ -0,0 +1,16 @@
+Template.comments.events({
+  "click #submitcomment": function(event){
+     console.log("hey we clicked it");
+     alert("Thank you for commenting!!");
+     const comment = $("#comment").val();
+     const date = new Date();
+     const jsonobj = {comment:comment, date:date}
+     Comments.insert(jsonobj);
+  }
+})
+
+
+Template.comments.helpers({
+
+  commentlist: function(){return Comments.find();}
+})
