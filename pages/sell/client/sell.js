8dd0c873c9ada3007120cdf4ea85dcb3cdafba5d
RicardoChino <Elprimitony@hotmail.com>
2016-11-15 16:53:02 -0500

    my changes

--- pages/sell/client/sell.js
+++ pages/sell/client/sell.js
@@ -10,9 +10,28 @@ Template.sell.events({
 		$('#description').val('');
 		$('#price').val('');
 		$('#photo').val('');
+	},
+	'click .edit':function(event) {
+		Session.set("selectedItemEdit",this);
+		$('#editModal').modal('toggle');
+	},
+	'click #saveEdit':function(event){
+		Items.update(Session.get("selectedItemEdit")._id, {
+	      $set: { price:$('#editPrice').val(), description:$('#editDescription').val(),photo:$('#editPhoto').val() },
+	    });
+	    $('#editModal').modal('toggle');
 	}
 })
 
 Template.sell.helpers({
-	myItems: function() { return Items.find({seller:Meteor.userId()}); }
+	myItems: function() { return Items.find({seller:Meteor.userId(),sold:false}); },
+	selectedPrice:function() {
+		return Items.findOne({_id:Session.get("selectedItemEdit")._id}).price;
+	},
+	selectedDescription:function() {
+		return Items.findOne({_id:Session.get("selectedItemEdit")._id}).description;
+	},
+	selectedPhoto:function() {
+		return Items.findOne({_id:Session.get("selectedItemEdit")._id}).photo;
+	}
 })
